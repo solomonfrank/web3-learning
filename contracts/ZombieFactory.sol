7905edaecda7334acb4fc00cc206a5c2883c64f2
solomonrock13 <solomon.rock@Africaprudential.com>
2022-03-17 12:26:12 +0100

    Add requre function

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -28,6 +28,7 @@ contract ZombieFactory {
             return rand % dnaModulus;
         }
         function createRandomZombie(string memory _name) public {
+            require( ownerZombieCount[msg.sender] == 0);
             uint randDna = _generateRandomDna(_name);
             _createZombies(_name, randDna);
     }
