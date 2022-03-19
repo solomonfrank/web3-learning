50345dc363326c26deb483157ebe3e1b63d30879
solomonrock13 <solomon.rock@Africaprudential.com>
2022-03-19 12:16:00 +0100

    Add interface and ability to interact with other contract external/public function

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -4,12 +4,40 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyinterface = KittyInterface(ckAddress);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage zombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (_targetDna + zombie.dna) / 2;
+        
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
         _createZombies("NoName", newDna);
     }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,, kittyDna) = kittyinterface.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
+    }
+}
+
+interface KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+);
 }
 
