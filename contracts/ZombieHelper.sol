90778bf1aebc3fa38bf7b2332b6a3a6bda86e99d
solomonrock13 <solomon.rock@Africaprudential.com>
2022-03-20 15:22:31 +0100

    Payable function has the capability to receive ether

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -6,10 +6,17 @@ import "./Zombiefeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
   // Start here
+  uint levelUpFee = 0.001 ether;
 modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
-    }
+}
+function levelUp(uint _zombieId) external payable {
+  require(msg.value == levelUpFee);
+  zombies[_zombieId].level++;
+
+
+}
     
 function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
